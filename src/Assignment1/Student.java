a1a328d02c22622e886d2d7444b7eb5259b0d501
hitencodes01 <hitenunnao@gmail.com>
2025-02-26 08:02:05 +0530

    making changes to optimised Student

--- src/Assignment1/Student.java
+++ src/Assignment1/Student.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Student {
     Scanner sc = new Scanner(System.in);
     float  m1, m2, m3 , average;
+    float maximum;
     int age;
     String name;
     public Student(int a , String b){
@@ -23,23 +24,25 @@ public class Student {
         average =  (m1+m2+m3)/3;
         System.out.println("the average = "+average);
     }
-    public void max(){
-       if(m1 > m2 && m1 > m3){System.out.println("the maximum marks "+m1);}
-       if(m2 > m1 && m2 > m3){System.out.println("the maximum marks "+m2);}
-       if(m3 > m2 && m3 > m1){System.out.println("the maximum marks "+m3);}
+    public void maxValue(){
+    //    if(m1 > m2 && m1 > m3){System.out.println("the maximum marks "+m1);}
+    //    if(m2 > m1 && m2 > m3){System.out.println("the maximum marks "+m2);}
+    //    if(m3 > m2 && m3 > m1){System.out.println("the maximum marks "+m3);}
+         this.maximum =Math.max(m1,Math.max(m2,m3));
+        System.out.println("the maximum marks = "+maximum);
     }
     public void info(){
         System.out.println("all information of student : ");
         System.out.printf("the %s of %d years\n",name,age);
         System.out.println("marks are = "+m1+", "+m2+", "+m3);
         avg();
-        max();
+        maxValue();
     }
     public static void main(String[] args) {
         Student st  = new Student(10,"xyz");
         st.setMarks();
         st.avg();
-        st.max();
+        st.maxValue();
         st.info();
     }
 }
